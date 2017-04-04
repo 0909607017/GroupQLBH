eb486807a4eaf8be1392b88a49d25d8f6ceb4322
quangtan <1451010172tan@ou.edu.vn>
2017-04-04 23:07:28 +0700

    Finish CaSi

---
+++ QLBH/BUS/BaiHat_BUS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DATA;
+
+namespace BUS
+{
+   public class BaiHat_BUS
+    {
+       BaiHat_DATA objBaiHat = new BaiHat_DATA();
+       public DataTable getBaiHat_by_mabaihat(string mabaihat)
+       {
+           return objBaiHat.getBaiHat_by_mabaihat(mabaihat);
+       }
+    }
+}
--- QLBH/QLBH/FrmMain.cs
+++ QLBH/QLBH/FrmMain.cs
@@ -17,6 +17,11 @@ namespace QLBH
             InitializeComponent();
         }
         private DataTable dtCasi;
+        private DataTable dtCasi_Baihat;
+        bool danapxong_lstBox = false;
+
+
+
         private void thôngTinSinhViênToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Các sinh viên thực hiện:\nPhạm Nguyễn Xuân Phúc - 1451010135\nTrần Dũng Danh - 1451010025\nLê Thanh Phước - 1451010143\nTrương Quang Tân - 1451010172");
@@ -33,7 +38,62 @@ namespace QLBH
         private void FrmMain_Load(object sender, EventArgs e)
         {
             load_Casi();
+            danapxong_lstBox = true;
       
         }
+
+
+
+
+        //xu li Click button listview listbox
+       
+        private void lstDanhSachCaSi_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (danapxong_lstBox)
+            {
+                if (lstDanhSachCaSi.SelectedItems.Count == 0)
+                    return;
+                //lvwBaiHat_CaSi.Items.Clear();
+                DataView dv = new DataView(dtCasi);
+                dv.RowFilter = "macasi = '" + lstDanhSachCaSi.SelectedValue + "'";
+                string tencasi = "";
+                foreach (DataRowView dr in dv)
+                {
+                    tencasi = lblTenCaSi.Text = dr["tencasi"].ToString();
+                    txtThongTinCaSi.Text = dr["thongtincasi"].ToString();
+                }
+                if (txtThongTinCaSi.Text.Trim().Equals(""))
+                    txtThongTinCaSi.Text = "Chưa có thông tin cho ca sĩ: [" + tencasi + "]";
+
+                dtCasi_Baihat = new Casi_Baihat_BUS().getCasi_BaiHat_by_macasi(lstDanhSachCaSi.SelectedValue.ToString());
+                lvwBaiHat_CaSi.Items.Clear();
+                foreach (DataRow dr in dtCasi_Baihat.Rows)
+                {
+                    DataTable dt = new BaiHat_BUS().getBaiHat_by_mabaihat(dr["mabaihat"].ToString());
+
+                    ListViewItem li = lvwBaiHat_CaSi.Items.Add("");
+                    li.SubItems.Add(dt.Rows[0]["tenbaihat"].ToString());
+                }
+                stt(lvwBaiHat_CaSi);
+
+            }
+        }
+
+
+
+        //Ham` load so thu tu
+        private void stt(ListView lvw)
+        {
+            for (int i = 1; i <= lvw.Items.Count; i++)
+            {
+                lvw.Items[i - 1].Text = i.ToString();
+                lvw.Items[i - 1].ImageIndex = 0;
+                if (i % 2 != 0)
+                    lvw.Items[i - 1].BackColor = Color.LightBlue;
+            }
+        }
     }
+
+
+    
 }
